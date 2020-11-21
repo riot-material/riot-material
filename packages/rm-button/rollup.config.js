fbdfaf9b9cb148185d3a92390f90737bcd93a808
Giuliano Collacchioni <eliteXXL@hotmail.it>
2020-11-21 22:46:31 +0100

    chore: implement @riot-material/rm-button

---
+++ packages/rm-button/rollup.config.js
@@ -0,0 +1,95 @@
+const riot = require("rollup-plugin-riot");
+const commonjs = require("@rollup/plugin-commonjs");
+const nodeResolve = require("@rollup/plugin-node-resolve").default;
+
+const globals = {
+    "@riot-material/position-controller": "riotMaterial.positionController",
+    "@riot-material/ripple": "riotMaterial.ripple",
+    "@riot-material/rm-icon": "riotMaterial.components.icon",
+    "riot": "riot"
+};
+const external = Object.keys(globals);
+
+export default [
+    {
+        input: "index.riot",
+        external,
+        plugins: [
+            nodeResolve(),
+            commonjs(),
+            riot()
+        ],
+        output: {
+            file: "index.js",
+            format: "cjs",
+            exports: "auto"
+        }
+    },
+    {
+        input: "index.riot",
+        external,
+        plugins: [
+            nodeResolve(),
+            commonjs(),
+            riot()
+        ],
+        output: {
+            file: "index.es.js",
+            format: "es"
+        }
+    },
+    {
+        input: "index.riot",
+        external,
+        plugins: [
+            nodeResolve(),
+            commonjs(),
+            riot()
+        ],
+        output: {
+            file: "index.amd.js",
+            format: "amd",
+            globals
+        }
+    },
+    {
+        input: "index.riot",
+        plugins: [
+            nodeResolve(),
+            commonjs(),
+            riot()
+        ],
+        output: {
+            file: "index.amd+libs.js",
+            format: "amd"
+        }
+    },
+    {
+        input: "index.riot",
+        external,
+        plugins: [
+            nodeResolve(),
+            commonjs(),
+            riot()
+        ],
+        output: {
+            name: "riotMaterial.components.button",
+            file: "index.umd.js",
+            format: "umd",
+            globals
+        }
+    },
+    {
+        input: "index.riot",
+        plugins: [
+            nodeResolve(),
+            commonjs(),
+            riot()
+        ],
+        output: {
+            name: "riotMaterial.components.button",
+            file: "index.umd+libs.js",
+            format: "umd"
+        }
+    }
+];
