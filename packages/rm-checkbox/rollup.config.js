99f67de1b97e1a7449c4dcc0f6b68863463a876c
Giuliano Collacchioni <eliteXXL@hotmail.it>
2020-11-21 22:54:29 +0100

    First commit

---
+++ packages/rm-checkbox/rollup.config.js
@@ -0,0 +1,92 @@
+const riot = require("rollup-plugin-riot");
+const commonjs = require("@rollup/plugin-commonjs");
+const nodeResolve = require("@rollup/plugin-node-resolve").default;
+
+const globals = {
+    "@riot-material/ripple": "riotMaterial.ripple",
+};
+const external = Object.keys(globals);
+
+export default [
+    {
+        input: "index.riot",
+        external,
+        plugins: [
+            nodeResolve(),
+            commonjs(),
+            riot()
+        ],
+        output: {
+            file: "index.js",
+            format: "cjs",
+            exports: "auto"
+        }
+    },
+    {
+        input: "index.riot",
+        external,
+        plugins: [
+            nodeResolve(),
+            commonjs(),
+            riot()
+        ],
+        output: {
+            file: "index.es.js",
+            format: "es"
+        }
+    },
+    {
+        input: "index.riot",
+        external,
+        plugins: [
+            nodeResolve(),
+            commonjs(),
+            riot()
+        ],
+        output: {
+            file: "index.amd.js",
+            format: "amd",
+            globals
+        }
+    },
+    {
+        input: "index.riot",
+        plugins: [
+            nodeResolve(),
+            commonjs(),
+            riot()
+        ],
+        output: {
+            file: "index.amd+libs.js",
+            format: "amd"
+        }
+    },
+    {
+        input: "index.riot",
+        external,
+        plugins: [
+            nodeResolve(),
+            commonjs(),
+            riot()
+        ],
+        output: {
+            name: "riotMaterial.components.checkbox",
+            file: "index.umd.js",
+            format: "umd",
+            globals
+        }
+    },
+    {
+        input: "index.riot",
+        plugins: [
+            nodeResolve(),
+            commonjs(),
+            riot()
+        ],
+        output: {
+            name: "riotMaterial.components.checkbox",
+            file: "index.umd+libs.js",
+            format: "umd"
+        }
+    }
+];
