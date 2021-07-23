67379b74c9c87bd3ef0af7d6893d317a00f418ce
Giuliano Collacchioni <eliteXXL@hotmail.it>
2021-07-23 23:03:54 +0200

    feat: change distribution formats

--- packages/app-bar-utils/rollup.config.js
+++ packages/app-bar-utils/rollup.config.js
@@ -6,31 +6,16 @@ export default [
         plugins: [
             typescript()
         ],
-        output: {
-            file: "dist/index.amd.js",
-            format: "amd"
-        }
-    },
-    {
-        input: "src/index.ts",
-        plugins: [
-            typescript()
-        ],
-        output: {
-            file: "dist/index.js",
-            exports: "auto",
-            format: "cjs"
-        }
-    },
-    {
-        input: "src/index.ts",
-        plugins: [
-            typescript()
-        ],
-        output: {
-            name: "riotMaterial.appBarUtils",
-            file: "dist/index.umd.js",
-            format: "umd"
-        }
+        output: [
+            {
+                name: "riotMaterial.appBarUtils",
+                file: "dist/index.js",
+                format: "umd"
+            },
+            {
+                file: "dist/index.es.js",
+                format: "es"
+            }
+        ]
     }
 ]
